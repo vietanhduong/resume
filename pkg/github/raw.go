f9dfa73b2dde584ce69546dead33ea5e1b2fbb95
Viet Anh Duong <vietanhs0817@gmail.com>
2021-07-05 23:24:13 +0700

    update doc and handler request error

--- pkg/github/raw.go
+++ pkg/github/raw.go
@@ -1,6 +1,7 @@
 package github
 
 import (
+	"errors"
 	"fmt"
 	"github.com/vietanhduong/resume/pkg/cerrors"
 	"io/ioutil"
@@ -23,10 +24,12 @@ func NewRaw(user, repo, branch string) *raw {
 	}
 }
 
+// baseURL generate GitHub Raw URL
 func (r *raw) baseURL() string {
 	return fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/%s", r.user, r.repo, r.branch)
 }
 
+// GetRaw make a request to GitHub to receive raw file
 func (r *raw) GetRaw(path string) ([]byte, *cerrors.CError) {
 	url := fmt.Sprintf("%s/%s", r.baseURL(), path)
 
@@ -34,12 +37,17 @@ func (r *raw) GetRaw(path string) ([]byte, *cerrors.CError) {
 	if err != nil {
 		return nil, cerrors.NewCError(http.StatusInternalServerError, err)
 	}
+	// make sure defer response body after made a request
 	defer func() {
 		if err := response.Body.Close(); err != nil {
 			log.Fatalf("close response body error, err: %v", err)
 		}
 	}()
-
+	// validate response code
+	if response.StatusCode != http.StatusOK {
+		return nil, cerrors.NewCError(response.StatusCode, errors.New(response.Status))
+	}
+	// read response body
 	content, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return nil, cerrors.NewCError(http.StatusInternalServerError, err)
@@ -48,6 +56,7 @@ func (r *raw) GetRaw(path string) ([]byte, *cerrors.CError) {
 	return content, nil
 }
 
+// SaveRaw write content to disk
 func (r *raw) SaveRaw(content []byte, output string) *cerrors.CError {
 	err := ioutil.WriteFile(output, content, os.FileMode(0644))
 	if err != nil {
